a9ddf6f25aee6358323f9182788792875de17e7d
ssab <qasw4554@gmail.com>
2019-11-20 23:55:02 +0900

    add

--- search/main.go
+++ search/main.go
@@ -12,7 +12,7 @@ func main() {
 		fmt.Printf("検索キーワードを1つ付け加えて再度実行してください.\n e.g. ./twitter-search abc\n")
 		os.Exit(1)
 	}
-	tweets, err := lib.AllSearch(lib.Authorize(), os.Args[1], true)
+	tweets, err := lib.AllSearch(lib.Authorize(), os.Args[1], false)
 	if err != nil {
 		fmt.Printf("error to AllSearch. error: %v\n", err)
 		os.Exit(1)
